adf40544cb5b5f5b4eda46a52520353f4499df8c
ruicalvario77 <ruic7777@gmail.com>
2025-03-14 09:29:19 +0000

    Remove dashboard header, rely on Navbar.tsx, fix overlap

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// /pocket-agency/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        animation: {
+          "fade-in": "fadeIn 0.5s ease-in",
+        },
+        keyframes: {
+          fadeIn: {
+            "0%": { opacity: "0" },
+            "100%": { opacity: "1" },
+          },
+        },
+      },
+    },
+    plugins: [],
+  };
