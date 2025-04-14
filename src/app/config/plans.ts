a3dd7cf89262103cc43cb596e199c7e8b21be2c3
ruicalvario77 <ruic7777@gmail.com>
2025-04-14 13:43:42 +0000

    Other

--- src/app/config/plans.ts
+++
@@ -1,6 +0,0 @@
-export const PLANS = {
-    basic: { name: "Basic", price: 3000 },
-    pro: { name: "Pro", price: 8000 },
-  } as const;
-  
-  export type PlanType = keyof typeof PLANS;
--- src/app/success/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SuccessPage() {
-    return (
-      <div>
-        <h1>Payment Successful!</h1>
-        <p>Thank you for subscribing to Pocket Agency. Check your email to associate your account.</p>
-      </div>
-    );
-  }
