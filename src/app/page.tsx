52422f2c7294253d06983b4f1cb7b46c99f3a570
ruicalvario77 <ruic7777@gmail.com>
2025-04-15 09:01:04 +0000

    404 Error Fix

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to Pocket Agency</h1>
+        <p>Unlimited Design & Development for Your Business</p>
+      </div>
+    );
+  }
