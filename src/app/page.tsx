dae9d75688791f27eef60f14123ebcfd3168ed68
ruicalvario77 <ruic7777@gmail.com>
2025-04-17 13:24:28 +0000

    Sentry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
-  throw new Error('Another test error for Sentry');
-  return <div>Hello World</div>;
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Pocket Agency</h1>
+      <p>Unlimited Design & Development for Your Business</p>
+    </div>
+  );
 }
